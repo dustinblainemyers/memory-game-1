578730b4cb6fa87714b764a69966ed8460a49abb
Peter Jones <peteflux@gmail.com>
2020-03-25 09:59:14 -0400

    added cards and styling to card now ready for part 2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import './App.css';
+import './components/MemoryCard.css';
+import MemoryCard from './components/MemoryCard';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Memory Game</h1>
-        <h2>Random Change</h2>
+        <h2>Match Cards To Win</h2>
       </header>
+      <div className="gameBoard">
+      <MemoryCard/>
+      <MemoryCard/>
+      <MemoryCard/>
+      <MemoryCard/>
+      <MemoryCard/>
+      <MemoryCard/>
+      <MemoryCard/>
+      <MemoryCard/>
+      <MemoryCard/>
+      <MemoryCard/>
+      <MemoryCard/>
+      <MemoryCard/>
+      <MemoryCard/>
+      <MemoryCard/>
+      <MemoryCard/>
+      <MemoryCard/>
+      </div>
     </div>
   );
 }
 
 export default App;
+
